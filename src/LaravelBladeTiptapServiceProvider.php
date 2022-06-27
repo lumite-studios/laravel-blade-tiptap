534eb3ab0f4628305674b04d27a9ccbbea1272ba
Paige Jones <hello@paigejones.me>
2022-06-27 08:32:30 +0100

    build: update composer

--- src/LaravelBladeTiptapServiceProvider.php
+++ src/LaravelBladeTiptapServiceProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace LumiteStudios\LaravelBladeTiptap;
 
 use Illuminate\Support\Facades\Blade;
@@ -16,15 +17,15 @@ class LaravelBladeTiptapServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
-		$this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-blade-tiptap');
+		$this->loadViewsFrom(__DIR__ . '/../resources/views', 'laravel-blade-tiptap');
 
 		Blade::component('tiptap-menu-item', TipTapMenuItem::class);
 		Blade::component('tiptap-menu', TipTapMenu::class);
 		Blade::component('tiptap-editor', TipTapEditor::class);
 
 		$this->publishes([
-			__DIR__.'/../resources/views' => resource_path('views/vendor/laravel-blade-tiptap'),
+			__DIR__ . '/../resources/views' => resource_path('views/vendor/laravel-blade-tiptap'),
 		]);
-        $this->publishes([__DIR__.'/../resources/dist' => public_path('vendor/laravel-blade-tiptap')], 'laravel-blade-tiptap');
+		$this->publishes([__DIR__ . '/../resources/dist' => public_path('vendor/laravel-blade-tiptap')], 'laravel-blade-tiptap');
 	}
 }
