551cb08910cba4b1c7ecd17b32b85fd163210b95
Paige Jones <hello@paigejones.me>
2022-06-27 08:44:23 +0100

    fix: use alpine raw for the menu buttons

--- resources/views/tiptap-menu-item.blade.php
+++ resources/views/tiptap-menu-item.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes }} @click.prevent="editor().chain().focus().{{ $action }}({{ json_encode($parameters) }}).run()">
+<button {{ $attributes }} @click.prevent="Alpine.raw(editor).chain().focus().{{ $action }}({{ json_encode($parameters) }}).run()">
 	{{ $slot }}
 </button>
